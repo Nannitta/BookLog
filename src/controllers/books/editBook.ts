8dd28c2ad3dba65708ae3b62b491311f4e3242df
Nanna <nanaa.gonzalez@gmail.com>
2024-01-13 15:36:27 +0100

    Add middleware to controll the number of photos

--- src/controllers/books/editBook.ts
+++ src/controllers/books/editBook.ts
@@ -1,5 +1,32 @@
-const editBook = () => {
+import { Request, Response, NextFunction } from 'express';
+import db from '../../db/initDbFirebase';
+import { throwError } from '../../helpers/errorHandler';
 
+const editBook = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { idBook } = req.params;
+    const { title, author, yearRelease, editorial, resume } = req.body;
+    let cover;
+
+    // Ver por que req.file o req.files o cover llega undefined
+    const checkedBook = await db.collection('books').where('__name__', '==', idBook).get();
+    
+    if (checkedBook.empty) {
+      throw throwError('El libro que quieres editar no existe', 400);
+    }    
+    
+    if (!title && !author && !yearRelease && !editorial && !resume && !cover) {
+      throw throwError('Debes modificar algún campo', 400);
+    }
+
+    res.status(200).send({
+      status: 'Ok',
+      message: 'Libro editado correctamente'
+    });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
 };
 
 export default editBook;
