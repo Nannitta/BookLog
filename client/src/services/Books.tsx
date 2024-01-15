d273e90107826c2c79d1146437e1fe9ffdcbe33e
Nanna <nanaa.gonzalez@gmail.com>
2024-01-15 10:52:42 +0100

    Add editBook component

--- client/src/services/Books.tsx
+++ client/src/services/Books.tsx
@@ -1,4 +1,5 @@
 import {BACK_API} from '@env';
+import { Book } from '../types/book.type';
 
 export const getAllBooks = async () => {
   const data = await fetch(`${BACK_API}`);
@@ -16,3 +17,36 @@ export const getBook = async (idBook: string) => {
 
   return book;
 };
+
+export const editBook = async (idBook: string, refreshBook: Book, cover: any) => {
+  const editBookForm = new FormData();
+  editBookForm.append('cover', cover);
+  if(refreshBook.title) {
+    editBookForm.append('title', refreshBook.title);
+  }
+  if(refreshBook.author) {
+    editBookForm.append('author', refreshBook.author);
+  }
+  if(refreshBook.editorial) {
+    editBookForm.append('editorial', refreshBook.editorial);
+  }
+  if(refreshBook.yearRelease) {
+    editBookForm.append('yearRelease', refreshBook.yearRelease);
+  }
+  if(refreshBook.resume) {
+    editBookForm.append('resume', refreshBook.resume);
+  }
+    
+  try {
+    await fetch(`${BACK_API}/edit/${idBook}`, {
+      method: 'PUT',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'multipart/form-data',
+      },
+      body: editBookForm
+    });   
+  } catch (error) {
+    console.log(error);
+  }  
+}; 
